55c4180f576652379ee7c694a7bda95d14f15153
Hui Chuan Lin <leon990x@gmail.com>
2020-08-30 12:31:52 -0500

    First Commit

---
+++ state1.js
@@ -0,0 +1,9 @@
+//State is a scene in your game
+
+var demo = {};
+demo.state1 = function(){};
+demo.state1.prototype = {
+    preload: function(){},
+    create: function(){},
+    update: function(){}
+};
